3acbed14218df17ac69dd240c03d0ef2f23f4678
Yiming Wang <yimingwang0916@gmail.com>
2022-11-01 18:08:17 +0100

    Update and rename Preprocess_training_data to Preprocess_training_data.py

--- Preprocess_training_data.py
+++ Preprocess_training_data.py
@@ -1,7 +1,9 @@
 os.getcwd()  
-os.chdir('/content/gdrive/My Drive/sentence_compression/data') 
-path = '/content/gdrive/My Drive/sentence_compression/data'
-os.listdir(path)
+os.chdir('/content/gdrive/My Drive/sentence_compression/data') # files' path
+
+# quantity process 
+#path = '/content/gdrive/My Drive/sentence_compression/data'
+#os.listdir(path)
 
 #datalist = []
 #for n in os.listdir(path):
@@ -11,7 +13,8 @@ os.listdir(path)
 src_sent_str = []
 src_head_str = []
 
-data_path = 'sent-comp.train01.json'
+data_path = 'sent-comp.train01.json' # file's name
+
 for i in range(1):
   with open(data_path, 'r', encoding="utf-8") as load:
     data = load.readlines()
@@ -20,11 +23,11 @@ for i in range(1):
       x = context.startswith("    \"sentence\":")  # to avoid those lines including “sentence” in the middle 
       y = context.startswith("  \"headline\":")    # to avoid those lines including “headline” in the middle 
       if x == True:
-        context = context.replace('\n', "").replace('\\n', "").replace('\\\n', "").replace('\\\\n', "").replace('\\\\\n', "")
+        context = context.replace('\n', "").replace('\\n', "").replace('\\\n', "").replace('\\\\n', "").replace('\\\\\n', "") # delete meaningless string
         con_list = context.split()                 # delete words, "sentence" and "headline" at the beginning of strings
         con_list.remove("\"sentence\":")
         if len(src_sent_str) < 1:                  # append only non-repeat sentences
-          src_sent_str.append(' '.join(con_list))
+          src_sent_str.append(' '.join(con_list))  
         else:
           con_check = ' '.join(con_list)
           if con_check != src_sent_str[-1]:
@@ -34,6 +37,13 @@ for i in range(1):
         con_list = context.split()
         con_list.remove("\"headline\":")
         src_head_str.append(' '.join(con_list))
-  print('finish extraction of ' + data_path)
+  print('-- finish extraction of ' + data_path)
 
-print('end')
+# save extracted text as new .json file
+df_sent = pd.DataFrame(src_sent_str,columns = ['sentence']) # save as .json
+df_head = pd.DataFrame(src_head_str,columns = ['headline'])
+df = pd.concat([df_sent, df_head],axis=1)
+print(df)
+data_save_path = "/content/gdrive/My Drive/sentence_compression/preprocessed_data/train01.json" 
+df.to_json(data_save_path, orient="index") 
+print('-- saved processed file as' + data_save_path)
